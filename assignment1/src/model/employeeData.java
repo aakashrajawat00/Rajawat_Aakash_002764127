2bf883fd270bc5285e6bc4f8cc91ffba1f3801f6
aakashrajawat00 <114462883+aakashrajawat00@users.noreply.github.com>
2022-09-27 15:54:05 -0400

    Add files via upload

---
+++ assignment1/src/model/employeeData.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+public class employeeData {
+  private ArrayList<humanResource> employee;
+
+    public employeeData() {
+        this.employee = new ArrayList<humanResource>();
+        
+    }
+
+    public ArrayList<humanResource> getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(ArrayList<humanResource> employee) {
+        this.employee = employee;
+    }
+    
+  
+  
+}
---
+++ assignment1/src/model/humanResource.java
@@ -0,0 +1,106 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author aakashrajawat
+ */
+public class humanResource {
+    private String name;
+    private int employeeId;
+    private int age;
+    private String Gender;
+    private String Level;
+    private String teamInfo;
+    private String postionTitle;
+    private String email;
+    private int phonenumber;
+    private Date currentDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+
+    public void setGender(String Gender) {
+        this.Gender = Gender;
+    }
+
+    public String getLevel() {
+        return Level;
+    }
+
+    public void setLevel(String Level) {
+        this.Level = Level;
+    }
+
+    public String getTeamInfo() {
+        return teamInfo;
+    }
+
+    public void setTeamInfo(String teamInfo) {
+        this.teamInfo = teamInfo;
+    }
+
+    public String getPostionTitle() {
+        return postionTitle;
+    }
+
+    public void setPostionTitle(String postionTitle) {
+        this.postionTitle = postionTitle;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(int phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public Date getCurrentDate() {
+        return currentDate;
+    }
+
+    public void setCurrentDate(Date currentDate) {
+        this.currentDate = currentDate;
+    }
+    
+    
+}
