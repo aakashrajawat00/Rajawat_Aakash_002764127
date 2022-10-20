f296cb5abf817757fe3c28cda44756666c977031
aakashrajawat00 <114462883+aakashrajawat00@users.noreply.github.com>
2022-10-20 19:32:11 -0400

    Add files via upload

---
+++ aedAssignment2/src/model/vitalSign.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author aakashrajawat
+ */
+public class vitalSign {
+    private int respiratoryRate;
+    private int heartRate;
+    private int bloodPressure;
+    private int weight;
+    private Date timestamp;
+
+    public int getRespiratoryRate() {
+        return respiratoryRate;
+    }
+
+    public void setRespiratoryRate(int respiratoryRate) {
+        this.respiratoryRate = respiratoryRate;
+    }
+
+    public int getHeartRate() {
+        return heartRate;
+    }
+
+    public void setHeartRate(int heartRate) {
+        this.heartRate = heartRate;
+    }
+
+    public int getBloodPressure() {
+        return bloodPressure;
+    }
+
+    public void setBloodPressure(int bloodPressure) {
+        this.bloodPressure = bloodPressure;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+    
+    
+}
---
+++ aedAssignment2/src/model/vitalSignhistory.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author aakashrajawat
+ */
+public class vitalSignhistory {
+    private ArrayList<vitalSign> history;
+
+    public ArrayList<vitalSign> getHistory() {
+        return history;
+    }
+
+    public void setHistory(ArrayList<vitalSign> history) {
+        this.history = history;
+    }
+    
+    
+}
